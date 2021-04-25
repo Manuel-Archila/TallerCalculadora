e8b31cd0cb77f6aff9298827bb9d70519dd7c839
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2021-04-25 17:35:49 -0600

    Creacion de todos los botones

--- app/src/main/java/com/example/application/Calculadora.kt
+++ app/src/main/java/com/example/application/Calculadora.kt
@@ -11,6 +11,18 @@ class Calculadora : AppCompatActivity() {
     lateinit var btn2: Button
     lateinit var btn3: Button
     lateinit var btn4: Button
+    lateinit var btn5: Button
+    lateinit var btn6: Button
+    lateinit var btn7: Button
+    lateinit var btn8: Button
+    lateinit var btn9: Button
+    lateinit var btn10: Button
+    lateinit var btn11: Button
+    lateinit var btn12: Button
+    lateinit var btn13: Button
+    lateinit var btn14: Button
+    lateinit var btn15: Button
+    lateinit var btn16: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +33,19 @@ class Calculadora : AppCompatActivity() {
         btn2 = findViewById(R.id.btn2)
         btn3 = findViewById(R.id.btn3)
         btn4 = findViewById(R.id.btn4)
+        btn5 = findViewById(R.id.btn5)
+        btn6 = findViewById(R.id.btn6)
+        btn7 = findViewById(R.id.btn7)
+        btn8 = findViewById(R.id.btn8)
+        btn9 = findViewById(R.id.btn9)
+        btn10 = findViewById(R.id.btn10)
+        btn11 = findViewById(R.id.btn11)
+        btn12 = findViewById(R.id.btn12)
+        btn13 = findViewById(R.id.btn13)
+        btn14 = findViewById(R.id.btn14)
+        btn15 = findViewById(R.id.btn15)
+        btn16 = findViewById(R.id.btn16)
+
 
         btn1.setOnClickListener {
             txtResultado.text = txtResultado.text as String + "1"
@@ -32,7 +57,45 @@ class Calculadora : AppCompatActivity() {
             txtResultado.text = txtResultado.text as String + "3"
         }
         btn4.setOnClickListener {
+            txtResultado.text = txtResultado.text as String + "+"
+        }
+        btn5.setOnClickListener {
             txtResultado.text = txtResultado.text as String + "4"
         }
+        btn6.setOnClickListener {
+            txtResultado.text = txtResultado.text as String + "5"
+        }
+        btn7.setOnClickListener {
+            txtResultado.text = txtResultado.text as String + "6"
+        }
+        btn8.setOnClickListener {
+            txtResultado.text = txtResultado.text as String + "-"
+        }
+        btn9.setOnClickListener {
+            txtResultado.text = txtResultado.text as String + "7"
+        }
+        btn10.setOnClickListener {
+            txtResultado.text = txtResultado.text as String + "8"
+        }
+        btn11.setOnClickListener {
+            txtResultado.text = txtResultado.text as String + "9"
+        }
+        btn12.setOnClickListener {
+            txtResultado.text = txtResultado.text as String + "*"
+        }
+        btn13.setOnClickListener {
+            txtResultado.text = txtResultado.text as String + "0"
+        }
+        btn14.setOnClickListener {
+            txtResultado.text = txtResultado.text as String + "."
+        }
+        btn15.setOnClickListener {
+
+            txtResultado.text = ""
+        }
+        btn16.setOnClickListener {
+            txtResultado.text = txtResultado.text as String + "/"
+        }
+
     }
 }
